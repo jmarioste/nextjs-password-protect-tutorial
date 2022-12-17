946945a52ae795fe6da0ea63f79b29c5d9ebb31d
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-17 19:37:39 +0800

    Fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
